1f7e2b1c58669269a76f3eb04f5592ca430d4c2a
abhishekibr2 <abhishekibr.trainee2@gmail.com>
2026-04-23 11:05:35 +0530

    fix:gtmcode

--- public-frontend/src/app/layout.tsx
+++ public-frontend/src/app/layout.tsx
@@ -38,14 +38,14 @@ export default function RootLayout({
 new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
 j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
 'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-})(window,document,'script','dataLayer','GTM-PBFK6Q8L');`}
+})(window,document,'script','dataLayer','GTM-5J4KB4PF');`}
         </Script>
       </head>
       <body>
         {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
-            src="https://www.googletagmanager.com/ns.html?id=GTM-PBFK6Q8L"
+            src="https://www.googletagmanager.com/ns.html?id=GTM-5J4KB4PF"
             height="0"
             width="0"
             style={{ display: "none", visibility: "hidden" }}
